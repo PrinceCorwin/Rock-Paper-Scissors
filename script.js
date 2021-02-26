2e2cb19281a49c0b190dfa7a1d83ea95e8707d07
Steve Corwin <scaccr@yahoo.com>
2021-02-26 08:53:28 -0600

    full 5 round play and winner added

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
-function game(userInput, compInput) {
-  if (compInput === userInput) {
-    return "tie";
-  } else if (
-    (compInput === "rock" && userInput === "scissors") ||
-    (compInput === "scissors" && userInput === "paper") ||
-    (compInput === "paper" && userInput === "rock")
-  ) {
-    return "Computer Wins";
+function round() {
+  let round = 1;
+  let pscore = 0;
+  let cscore = 0;
+  for (let i = 1; i < 6; i++) {
+    console.log("Round: " + round);
+    const userInput = prompt(
+      `enter "rock", "paper", or "scissors"`
+    ).toLowerCase();
+    let compInput = Math.floor(Math.random() * 3);
+    switch (compInput) {
+      case 0:
+        compInput = "rock";
+        break;
+      case 1:
+        compInput = "paper";
+        break;
+      case 2:
+        compInput = "scissors";
+        break;
+    }
+    console.log("Computer chooses: " + compInput);
+    console.log("Player chooses: " + userInput);
+    console.log(game(userInput, compInput));
+    console.log("SCORE Player: " + pscore + " Computer: " + cscore);
+    round++;
+  }
+
+  function game(userInput, compInput) {
+    if (compInput === userInput) {
+      return "tie";
+    } else if (
+      (compInput === "rock" && userInput === "scissors") ||
+      (compInput === "scissors" && userInput === "paper") ||
+      (compInput === "paper" && userInput === "rock")
+    ) {
+      cscore++;
+      return "Computer Wins";
+    } else {
+      pscore++;
+      return "Player Wins";
+    }
+  }
+  if (pscore === cscore) {
+    console.log("Tie Score!");
+  } else if (pscore < cscore) {
+    console.log("Computer Wins!");
   } else {
-    return "Player Wins";
+    console.log("Player Wins!");
   }
 }
-const userInput = prompt(`enter "rock", "paper", or "scissors"`).toLowerCase();
-let compInput = Math.floor(Math.random() * 3);
-// console.log("comp before switch: " + compInput);
-switch (compInput) {
-  case 0:
-    compInput = "rock";
-    break;
-  case 1:
-    compInput = "paper";
-    break;
-  case 2:
-    compInput = "scissors";
-    break;
-}
-let compare = [compInput, userInput];
-// console.log("compare: " + compare);
-let compWin = [
-  ["rock", "scissors"],
-  ["paper", "rock"],
-  ["scissors", "paper"],
-];
-
-console.log("Computer chooses: " + compInput);
-console.log("Player chooses: " + userInput);
-
-// console.log(compWin.indexOf(compare));
-console.log(game(userInput, compInput));
+round();
