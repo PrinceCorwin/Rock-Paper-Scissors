7c94cb16d2092a4af8c182c22c1d5305d2e8b1c9
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2021-02-28 20:11:48 -0600

    Added event listeners to icons

--- script.js
+++ script.js
@@ -1,52 +1,71 @@
-function round() {
-  let round = 1;
-  let pscore = 0;
-  let cscore = 0;
-  for (let i = 1; i < 6; i++) {
-    console.log("Round: " + round);
-    // const userInput = prompt(
-    //   `enter "rock", "paper", or "scissors"`
-    ).toLowerCase();
-    let compInput = Math.floor(Math.random() * 3);
-    switch (compInput) {
-      case 0:
-        compInput = "rock";
-        break;
-      case 1:
-        compInput = "paper";
-        break;
-      case 2:
-        compInput = "scissors";
-        break;
-    }
-    console.log("Computer chooses: " + compInput);
-    console.log("Player chooses: " + userInput);
-    console.log(game(userInput, compInput));
-    console.log("SCORE Player: " + pscore + " Computer: " + cscore);
-    round++;
-  }
+let winner = "";
+let pscore = 0;
+let cscore = 0;
+let listen = document.querySelectorAll(".btn");
+for (let i = 0; i < listen.length; i++) {
+  listen[i].addEventListener("click", function () {
+    console.log(this.id);
+    game(this.id);
+  });
+}
+// SINGLE GAME FUNCTION
 
-  function game(userInput, compInput) {
-    if (compInput === userInput) {
-      return "tie";
-    } else if (
-      (compInput === "rock" && userInput === "scissors") ||
-      (compInput === "scissors" && userInput === "paper") ||
-      (compInput === "paper" && userInput === "rock")
-    ) {
-      cscore++;
-      return "Computer Wins";
-    } else {
-      pscore++;
-      return "Player Wins";
-    }
+function game(userInput) {
+  if (winner != "") {
+    document.getElementById("victor").textContent =
+      "First to 5: Make Another Selection";
+    document.getElementById("victor").style.fontSize = "1.5rem";
   }
-  if (pscore === cscore) {
-    console.log("Tie Score!");
-  } else if (pscore < cscore) {
-    console.log("Computer Wins!");
+  let compInput = Math.floor(Math.random() * 3);
+  switch (compInput) {
+    case 0:
+      compInput = "rock";
+      break;
+    case 1:
+      compInput = "paper";
+      break;
+    case 2:
+      compInput = "scissors";
+      break;
+  }
+  console.log("comp: " + compInput);
+  if (compInput === userInput) {
+    document.getElementById("winner").textContent = "TIE!";
+    setScore(pscore, cscore);
+    console.log("Tie");
+  } else if (
+    (compInput === "rock" && userInput === "scissors") ||
+    (compInput === "scissors" && userInput === "paper") ||
+    (compInput === "paper" && userInput === "rock")
+  ) {
+    cscore++;
+    setScore(pscore, cscore);
+    document.getElementById("winner").textContent = "THE TERMINATOR WON";
+
+    console.log("Computer Wins" + cscore + pscore);
   } else {
-    console.log("Player Wins!");
+    pscore++;
+    setScore(pscore, cscore);
+    document.getElementById("winner").textContent = "THE RESISTANCE WON";
+    console.log("Player Wins" + pscore + cscore);
+  }
+  if (pscore >= 5 || cscore >= 5) {
+    if (pscore > cscore) {
+      winner = "THE RESISTANCE";
+      console.log(winner);
+    } else {
+      winner = "THE TERMINATOR";
+      console.log(winner);
+    }
+    pscore = 0;
+    cscore = 0;
+    const vic = document.getElementById("victor");
+    vic.textContent = winner + " IS VICTORIOUS!";
+    vic.style.fontSize = "4rem";
   }
 }
-round();
+function setScore() {
+  document.getElementById("pscore").textContent = "Resistance: " + pscore;
+  document.getElementById("cscore").textContent = "Terminator: " + cscore;
+  return;
+}
